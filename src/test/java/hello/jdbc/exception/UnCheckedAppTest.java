bf70491994e327c705c8ad7e27466425b00cbccb
jungwon <mac20010@naver.com>
2023-08-06 00:02:08 +0900

    FEAT : 런타임 예외 적용

--- src/test/java/hello/jdbc/exception/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/UnCheckedAppTest.java
@@ -8,14 +8,27 @@ import hello.jdbc.exception.CheckedAppTest.Repository;
 import hello.jdbc.exception.CheckedAppTest.Service;
 import java.net.ConnectException;
 import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class UnCheckedAppTest {
     @Test
     void unchecked(){
         CheckedAppTest.Controller controller = new CheckedAppTest.Controller();
         assertThatThrownBy(()->controller.request())
                 .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    void printEx(){
+        Controller controller =new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info("ex",e);
+        }
+
     }
 
     static class Controller{
